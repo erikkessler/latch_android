eb35c4970790ce436a41a7454c5573b82e3d42d7
Erik Kessler <erik.kessler1@gmail.com>
2014-07-20 15:23:29 -0400

    empty app

--- app/src/main/java/com/inspiredo/latch/TodayActivity.java
+++ app/src/main/java/com/inspiredo/latch/TodayActivity.java
@@ -1,36 +1,58 @@
 package com.inspiredo.latch;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 
 public class TodayActivity extends Activity {
+
+    /**
+     * Certain screens allow for dual pane layout
+     */
+    boolean mDualPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_today);
+
+        // Setup the list
+
+
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.today, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                // TODO: Settings Activity
+                return true;
+            case R.id.action_add:
+                // TODO: Add Habit Activity
+                return true;
+            default:
+                Toast.makeText(this, "Unimplemented action", Toast.LENGTH_SHORT)
+                        .show();
+                return true;
         }
-        return super.onOptionsItemSelected(item);
     }
+
 }
