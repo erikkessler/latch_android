548a81a970eec7ae048bd0be2dccaad6db81b29b
Erik Kessler <erik.kessler1@gmail.com>
2014-08-10 13:21:50 -0400

    Notification opens app

--- app/src/main/java/com/inspiredo/latch/TriggerIntentService.java
+++ app/src/main/java/com/inspiredo/latch/TriggerIntentService.java
@@ -3,6 +3,7 @@ package com.inspiredo.latch;
 import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
@@ -20,6 +21,14 @@ public class TriggerIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        Intent resultIntent = new Intent(this, TodayActivity.class);
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
         String seqName = intent.getStringExtra(SEQUENCE_TITLE);
         NotificationManager mgr = (NotificationManager)
                 getSystemService(Context.NOTIFICATION_SERVICE);
@@ -31,7 +40,9 @@ public class TriggerIntentService extends IntentService {
                 seqName + " sequence")
                 .setSmallIcon(R.drawable.ic_stat_notification)
                 .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                .setContentIntent(resultPendingIntent)
                 .build();
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
         mgr.notify(1, notification);
     }
 }
